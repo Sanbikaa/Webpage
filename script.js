e28569b3c4a16ebbf477d21b9e6c7088e2cd3c6e
Sanbikaa <gouk77@gmail.com>
2021-07-03 14:20:42 -0500

    yessss

--- script.js
+++ script.js
@@ -9,14 +9,3 @@ if (confirm("How are you feeling")) {
 }
 var userName = prompt("What's your name?");
 console.log(userName);
-
-for (i = 0; i < 10; i = i+i){
-    console.log(i);
-}
-
-attempts = 0;
-
-while(attempts < 5){
-    prompt("Enter a number");
-    attempts = attempts + 1;
-}
