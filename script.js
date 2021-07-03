68122e9f2b79f3b357f6a4460dd0affad06e75f1
Sanbikaa <gouk77@gmail.com>
2021-07-03 14:16:34 -0500

    Code

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-confirm('This is a WIP the website will change ideas and titles overtime');
+alert("Welcome");
+if (confirm("How are you feeling")) {
+    var txt = "Good!";
+    alert(txt);
+    console.log(txt);
+} else {
+    var txt ="Hmmmm Alright"
+    console.log(txt);
+}
+var userName = prompt("What's your name?");
+console.log(userName);
 
-prompt('How are you?');
+for (i = 0; i < 10; i = i+i){
+    console.log(i);
+}
 
-alert('You sure about this?')
+attempts = 0;
+
+while(attempts < 5){
+    prompt("Enter a number");
+    attempts = attempts + 1;
+}
