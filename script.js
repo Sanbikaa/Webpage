14284fa5da3bf7e0e77c85a737c9cc14a5fc1035
Sanbikaa <gouk77@gmail.com>
2021-07-05 13:19:02 -0500

    java

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ if (confirm("How are you feeling")) {
 var userName = prompt("What's your name?");
 console.log(userName);
 
-var amount = prompt("How many Bitcoins would you like?");
+var amount = prompt("How many skates would you like?");
 for(let i = 0; i < amount; i++){
     document.write("<img src=https://houstonskatesports.com/product_images/uploaded_images/img-1207-2.jpg?t=1622488726&_ga=2.42734125.943112805.1622425458-1367785472.1622425458'>");
 }
